223e84dcdf1e5b0d930a20fa9ad71852aa46a2ab
ruthrb <rubor4898@oslomet.no>
2024-04-24 17:24:23 +0200

    javaendringer

--- src/main/java/com/example/oblig3ny/Oblig3.java
+++ src/main/java/com/example/oblig3ny/Oblig3.java
@@ -7,37 +7,76 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "Billett")
 
 public class Oblig3 {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-    private int id;
     private String film;
-    private int antall;
+    private String antall;
     private String fornavn;
     private String etternavn;
     private String telefonNr;
     private String epost;
 
-    @Override
-    public String toString() {
-        return "Oblig3{" +
-                "id=" + id +
-                ", film='" + film + '\'' +
-                ", antall='" + antall + '\'' +
-                ", fornavn='" + fornavn + '\'' +
-                ", etternavn='" + etternavn + '\'' +
-                ", telefonNr='" + telefonNr + '\'' +
-                ", epost='" + epost + '\'' +
-                '}';
+    public Oblig3(String film, String antall, String fornavn, String etternavn, String telefonNr, String epost) {
+        this.film = film;
+        this.antall = antall;
+        this.fornavn = fornavn;
+        this.etternavn = etternavn;
+        this.telefonNr = telefonNr;
+        this.epost = epost;
+    }
+
+    public Oblig3() {
+    }
+
+    public String getFilm() {
+        return film;
+    }
+
+    public void setFilm(String film) {
+        this.film = film;
+    }
+
+    public String getAntall() {
+        return antall;
+    }
+
+    public void setAntall(String antall) {
+        this.antall = antall;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public void setFornavn(String fornavn) {
+        this.fornavn = fornavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    public void setEtternavn(String etternavn) {
+        this.etternavn = etternavn;
+    }
+
+    public String getTelefonNr() {
+        return telefonNr;
+    }
+
+    public void setTelefonNr(String telefonNr) {
+        this.telefonNr = telefonNr;
+    }
+
+    public String getEpost() {
+        return epost;
+    }
+
+    public void setEpost(String epost) {
+        this.epost = epost;
     }
 }
+
+
 
 
